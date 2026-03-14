7b8b44f4039124bbadc8f612ef07b0629060ef2e
Maksymilian Dżugan <dzugan.maks@gmail.com>
2026-03-14 18:14:19 +0100

    chore(js): remove unused crpyto.js file and duplicated import in index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
 import './chart.js';
-import './crypto.js';
 import './events.js';
-import './render/renderApp.js';
 import { initApp } from './render/renderApp.js';
 import { initController } from './state/controller.js';
 
